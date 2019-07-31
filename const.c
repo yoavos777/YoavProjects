0a796f9f70bed1a7761e133d23432738f3cfac45
yoavos777 <yoav.morgenstern@gmail.com>
2019-07-31 20:02:11 +0300

    Update const.c

--- const.c
+++ const.c
@@ -11,7 +11,8 @@ int i;
 int j;
 char c;
 
-void build_arr()
+/*function that build the command array*/
+void build_command_arr()
 {
 	for(i=0;i<16;i++)
 	{
@@ -22,46 +23,48 @@ void build_arr()
 			comm_oparr[i].orig_addr[j] = -1;
 	}
 
-	for(i=0;i<2;i++){
+	for(i=0;i<2;i++){/*legal destnation method for the commands not,clr*/
 		for(j=1;j<4;j++)
 			comm_oparr[i+4].des_addr[j] = j;
 	}
 
 	for(i=0;i<7;i++){
-		if(i==2 || i==3 || i==6)
+		if(i==2 || i==3 || i==6)/*legal destnation method for the commands inc,dec,red*/
 			for(j=1;j<4;j+=2)
+
 				comm_oparr[i+7].des_addr[j] = j;
-		else if(i == 5)
+		else if(i == 5)/*legal destnation method for the commands prn*/
 			for(j=0;j<4;j++)
 				comm_oparr[i+7].des_addr[j] = j;
-		else
+
+		else/*legal destnation method for the commands jmp,bne,jsr*/
 			for(j=1;j<4;j++)
 				comm_oparr[i+7].des_addr[j] = j;
 	}
 
 	for(i=0;i<4;i++){
-		if(i == 1)
+		if(i == 1)/*legal destnation method for the commands cmp*/
 			for(j=0;j<4;j++)
 				comm_oparr[i].des_addr[j] = j;
-		else
+		else/*legal destnation method for the commands mov,add,sub*/
 			for(j=1;j<4;j++)
 				comm_oparr[i].des_addr[j] = j;
 	}
 
-	for(j=1;j<4;j++)
+	for(j=1;j<4;j++)/*legal destnation method for the commands lea*/
 		comm_oparr[6].des_addr[j] = j;
 	
 		
-	for(i=0;i<4;i++){
+	for(i=0;i<4;i++){/*legal origin method for the commands mov,cmp,add,sub*/
 		for(j=0;j<4;j++)
 			comm_oparr[i].orig_addr[j] = j;
 	}
 
-	for(j=1;j<3;j++)
+	for(j=1;j<3;j++)/*legal origin method for the commands lea*/
 		comm_oparr[6].orig_addr[j] = j;
 }
 
-
+/*build the ascii code array*/
 void build_ascii()
 {
 	i = 0;
